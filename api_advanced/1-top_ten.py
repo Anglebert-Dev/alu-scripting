4d2f10837e2ec9e44161a472dd074c86cb70c0aa
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2025-07-07 16:13:55 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10 hot posts from a subreddit"""
+"""Print the titles of the first 10 Hot Posts"""
 import requests
 
 
@@ -7,22 +7,14 @@ def top_ten(subreddit):
     """The top ten titles"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code != 200:
-        print("None")
-        return
-
-    json_data = response.json()
-    posts = json_data.get('data', {}).get('children', [])
-
-    if not posts:
-        print("None")
-        return
-
-    for post in posts:
-        title = post.get('data', {}).get('title')
-        if title:
-            print(title)
+    if response.status_code == 200:
+        try:
+            posts = response.json().get('data', {}).get('children', [])
+            for i in range(min(10, len(posts))):
+                print(posts[i].get('data', {}).get('title'))
+        except Exception:
+            print(None)
+    else:
+        print(None)
