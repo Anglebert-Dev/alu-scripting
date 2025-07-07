ca89644725aaaeaad3c1cae868dfaf6548d4c3c4
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2025-07-07 16:12:30 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+"""Print the titles of the first 10 hot posts from a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
     """The top ten titles"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {'limit': 10}
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-    else:
-        print(None)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
+        return
+
+    json_data = response.json()
+    posts = json_data.get('data', {}).get('children', [])
+
+    if not posts:
+        print("None")
+        return
+
+    for post in posts:
+        title = post.get('data', {}).get('title')
+        if title:
+            print(title)
