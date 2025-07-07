9dc1f8e05ef864254694cafaf0f9efb8b1d66345
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2025-07-07 16:10:01 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,22 @@
-#!/usr/bin/python3
-"""Print the titles of the first 10 hot posts from a subreddit"""
+"""Print the titles of the first 10 hot posts of a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the top 10 hot posts for a given subreddit"""
+    """Print the top 10 hot post titles of a given subreddit"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    params = {'limit': 10}
-
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
             print(None)
             return
 
-        data = response.json().get('data', {}).get('children', [])
-        if not data:
-            print(None)
-            return
+        data = response.json().get('data', {})
+        posts = data.get('children', [])
 
-        for post in data:
-            print(post.get('data', {}).get('title'))
-
+        for i in range(min(10, len(posts))):
+            title = posts[i].get('data', {}).get('title')
+            print(title)
     except Exception:
-        print(None)
