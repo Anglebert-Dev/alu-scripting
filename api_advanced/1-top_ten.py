6dadcf060c790928f6ddcb363905b659bf677f55
Anglebert-Dev <anglebertsh@gmail.com>
2025-07-18 12:02:11 +0200

    top 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,23 +5,38 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the top 10 hot posts for a given subreddit"""
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    if not subreddit:
+        print(None)
+        return
+    
+    headers = {'User-Agent': 'MyRedditApp/0.0.1'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'limit': 10}
-
+    
     try:
         response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        
         if response.status_code != 200:
             print(None)
             return
-
-        data = response.json().get('data', {}).get('children', [])
-        if not data:
+        
+        data = response.json()
+        
+        # Check if valid subreddit response (has expected structure)
+        if 'data' not in data or 'children' not in data['data']:
             print(None)
             return
-
-        for post in data:
-            print(post.get('data', {}).get('title'))
-
+        
+        posts = data['data']['children']
+        
+        if not posts:
+            print(None)
+            return
+        
+        # Print titles of up to 10 posts
+        for post in posts:
+            title = post['data']['title']
+            print(title)
+    
     except Exception:
         print(None)
